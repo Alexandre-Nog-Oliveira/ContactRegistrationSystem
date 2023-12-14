8037cccb9a44529cacac453d7b6e5dbe15c4f7d4
Alexandre Oliveira <Alexandre2001.oliveira@gmail.com>
2023-12-13 22:27:17 -0300

    Initializing login screen creation

--- ControleContatos/Models/UsuarioModel.cs
+++ ControleContatos/Models/UsuarioModel.cs
@@ -25,6 +25,6 @@ namespace ControleContatos.Models
         [Required(ErrorMessage = "Informe o perfil do usuário")]
         public PerfilEnum? Perfil {  get; set; }
         public DateTime DataCadastro { get; set; }
-        public DateTime DataAtualizacao { get; set; }
+        public DateTime? DataAtualizacao { get; set; }
     }
 }
